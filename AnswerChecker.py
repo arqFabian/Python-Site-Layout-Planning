949a5454233a35b0d567e164a94dace7ba11ca50
arqFabian <72966763+arqFabian@users.noreply.github.com>
2023-06-23 16:55:28 +0900

    new checker answer function and update on data entry for the answer in the answer_evaluation.py

--- AnswerChecker.py
+++ AnswerChecker.py
@@ -8,8 +8,34 @@ NUMBER_SOLUTIONS_TO_PLOT = 5
 
 #blender_file_path = "/Users/arqfa/OneDrive/Desktop/Research"
 blender_file_path = "/Users/arqfa/OneDrive - Kyushu University/ResearchBigData/VR-research/BlenderFiles"
-site_data_path = "/Site1"
-answer = 99
-scores_coordinates_sorted = np.load(blender_file_path + site_data_path + '/scores_coordinates_sorted.npy', allow_pickle=True)
-print("sorted score values successfully loaded")
-print(scores_coordinates_sorted[(int(answer))-1])
+#site_data_path = "/Site"
+#scores_coordinates_sorted = np.load(blender_file_path + site_data_path + '/scores_coordinates_sorted.npy', allow_pickle=True)
+#print("sorted score values successfully loaded")
+
+
+answer1 = [11, 8, 69, 15, 14, 43, 8, 8, 5, 1, 44, 20, 99, 8, 3, 75, 46]
+answer2 = [6, 35, 6, 63, 3, 1, 1, 1, 82, 14, 6]
+answer3 = [10, 1, 11, 38, 1, 83, 2, 3, 97, 20, 66]
+
+
+
+
+def answer_checker(answer_input, site_data_path_input, blender_file_path_input):
+    scores_coordinates_sorted = np.load(blender_file_path_input + site_data_path_input + '/scores_coordinates_sorted.npy',
+                                        allow_pickle=True)
+    print("Sorted score values successfully loaded")
+    print(f"coordinates for {site_data_path_input}")
+    for i in answer_input:
+        if i <= len(scores_coordinates_sorted):
+
+            print(scores_coordinates_sorted[i-1][8])
+        else:
+            print(f"Answer {i} is out of range.")
+
+# Call the answer_checker function with the desired inputs
+answer_checker(answer1, "/Site1", blender_file_path)
+answer_checker(answer2, "/Site2", blender_file_path)
+answer_checker(answer3, "/Site3", blender_file_path)
+
+
+
--- answer_evaluation.py
+++ answer_evaluation.py
@@ -21,12 +21,53 @@ z_level = int(site_information[6])
 
 # Data for Site 1 : L1 - experiment
 participant = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-blender_answer = [(61, -16, 0), (-21, 11, 0), (62, 28, 0), (9, 21, 0), (10, 21, 0), (58, -2, 0), (-28, -7, 0), (-24, -14, 0), (9,24,0), (28, -27, 0), (-8,6,0), (-18,-14,0), (-16, 13, 0)]
-vr_answer = [(6, -45, 0), (31, 20, 0), (36, -15, 0), (36, -13, 0), (34, -36, 0), (6, -44, 0), (31, 20, 0), (31, 20, 0), (9,1,0), (35,-35,0), (7,-44,0), (1,-11,0), (35,-37,0)]
 
-recommended_answer = (35, -35, 0)
+# Blender1 list
+blender_answer1 = [(61, -16, 0), (-21, 11, 0), (62, 28, 0), (9, 21, 0), (10, 21, 0), (58, -2, 0), (-28, -7, 0),
+                   (-24, -14, 0), (9, 24, 0), (28, -27, 0), (-8, 6, 0), (-16, 13, 0), (-18, -14, 0), (24, 9, 0),
+                   (24, 31, 0), (64, -4, 0), (-10, 13, 0)]
+# Blender1 list
+blender_answer2 = [(-8, -24, 0), (-58, -17, 0), (-7, -26, 0), (11, -26, 0), (-7, -24, 0), (-7, -26, 0), (-8, -23, 0),
+                   (-4, -27, 0), (-7, -25, 0), (18, -27, 0), (2, -21, 0)]
+# Blender1 list
+blender_answer3 = [(-50, 3, 0), (-51, -1, 0), (-51, 5, 0), (-51, 2, 0), (-55, -2, 0), (-52, 1, 0), (-52, -1, 0),
+                   (-52, -79, 0), (-52, -1, 0), (-51, -1, 0), (-20, 37, 0)]
 
-answers = list(zip(blender_answer, vr_answer))
+# VR1 list
+vr_answer1 = [(6.0, -45.0, 0.0), (31.0, 20.0, 0.0), (36.0, -15.0, 0.0), (36.0, -13.0, 0.0),
+       (34.0, -36.0, 0.0), (6.0, -44.0, 0.0), (31.0, 20.0, 0.0), (31.0, 20.0, 0.0),
+       (9.0, 1.0, 0.0), (35.0, -35.0, 0.0), (7.0, -44.0, 0.0), (35.0, -37.0, 0.0),
+       (1.0, -11.0, 0.0), (31.0, 20.0, 0.0), (34.0, -35.0, 0.0), (31.0, 23.0, 0.0),
+       (30.0, -4.0, 0.0)]
+
+# VR2 list
+vr_answer2 = [(12.0, -13.0, 0.0), (58.0, -7.0, 0.0), (12.0, -13.0, 0.0), (1.0, 37.0, 0.0),
+       (10.0, 16.0, 0.0), (-3.0, 20.0, 0.0), (-3.0, 20.0, 0.0), (-3.0, 20.0, 0.0),
+       (3.0, 8.0, 0.0), (-10.0, 31.0, 0.0), (12.0, -13.0, 0.0)]
+
+# VR3 list
+vr_answer3 = [(-26.0, 42.0, 0.0), (-26.0, 40.0, 0.0), (-20.0, 0.0, 0.0), (-25.0, 39.0, 0.0),
+       (-26.0, 40.0, 0.0), (-35.0, 12.0, 0.0), (-27.0, -32.0, 0.0), (-26.0, 39.0, 0.0),
+       (-19.0, 27.0, 0.0), (-30.0, 0.0, 0.0), (-22.0, 32.0, 0.0)]
+
+#recommended answers per site
+
+#Site1 - Rank1
+recommended_answer1 = (35, -35, 0)
+
+#Site2 - Rank1
+recommended_answer2 = (-3.0, 20.0, 0.0)
+
+#Site3 - Rank1
+recommended_answer3 = (-26.0, 40.0, 0.0)
+
+# combination of answers for blender and VR in a single list for graph and calculations
+
+answers1 = list(zip(blender_answer1, vr_answer1))
+
+answers2 = list(zip(blender_answer2, vr_answer2))
+
+answers3 = list(zip(blender_answer3, vr_answer3))
 
 
 # function to calculate the difference on the distance between answers in relation to the recommended answer
@@ -44,16 +85,18 @@ def accuracy_function(answer_input, recommended_answer_input):
         # calculate the distance using the distance formula
         distance = round((math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)), 3)
         distance_blender.append(distance)
-    print(f"The distances in blender from the locations chosen by the participants to the best location identified by the system are: "
-        f"{distance_blender}")
+    #print(f"The distances in blender from the locations chosen by "
+    #      f"the participants to the best location identified by the system are: "
+    #      f"{distance_blender}")
     for i in unity:
         x1, y1, z1 = i
 
         # calculate the distance using the distance formula
         distance = round((math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)), 3)
         distance_unity.append(distance)
-    print(f"The distances in Unity from the locations chosen by the participants to the best location identified by the system are: "
-          f"{distance_unity}")
+    #print(f"The distances in Unity from the locations chosen by "
+    #      f"the participants to the best location identified by the system are: "
+    #      f"{distance_unity}")
     accuracy_improvement = []
     for i in range(len(distance_blender)):
         improvement = round((float(distance_blender[i]) - float(distance_unity[i])) * 100 / float(distance_blender[i]),
@@ -65,7 +108,10 @@ def accuracy_function(answer_input, recommended_answer_input):
     return accuracy_improvement
 
 
-accuracy_percentage_improvement = accuracy_function(answers, recommended_answer)
+accuracy_percentage_improvement1 = accuracy_function(answers1, recommended_answer1)
+accuracy_percentage_improvement2 = accuracy_function(answers2, recommended_answer2)
+accuracy_percentage_improvement3 = accuracy_function(answers3, recommended_answer3)
+
 
 
 # function to plot the accuracy graph
@@ -157,8 +203,9 @@ def accuracy_graph(answer_input, recommended_answer_input):
     return
 
 
-accuracy_graph(answers, recommended_answer)
-
+accuracy_graph(answers1, recommended_answer1)
+accuracy_graph(answers2, recommended_answer2)
+accuracy_graph(answers3, recommended_answer3)
 
 def accuracy_bar_chart(available_position_list, y_original_value, y_label, number_of_plotted_solutions):
     # Create a new figure for the column graph
